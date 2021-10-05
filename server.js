31b6de4acd000e5f4266b47b4295ac2d208260a3
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-10-05 22:49:33 +0500

    worked

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const exphbs=require('express-handlebars')
 
 const app=express()
+
+app.engine('.hbs',exphbs({
+    defaultLayout:'main',
+    extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
 
 app.listen(3000,()=>{
     console.log('Server running on port:3000');
