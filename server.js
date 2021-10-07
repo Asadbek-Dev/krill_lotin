1aa94a0cbfbf514ce9e39143796fc4f38da66f0d
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-10-07 19:22:34 +0500

    styled

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const exphbs=require('express-handlebars')
+const path = require('path')
 
 const app=express()
 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
 app.engine('.hbs',exphbs({
     defaultLayout:'main',
     extname: '.hbs'
