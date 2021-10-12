15e5bb201a8a5901457e7100387f5e32ec96a147
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-10-12 17:26:20 +0500

    added async function

--- routes/translate.js
+++ routes/translate.js
@@ -1,6 +1,14 @@
 const { Router } = require('express')
 const router = Router()
 
+async function translateCL(word){
+  try {
+    let result=''
+  } catch (er) {
+    console.log(err);
+  }
+}
+
 router.get('/',(req,res)=>{
   res.render('index')
 })
